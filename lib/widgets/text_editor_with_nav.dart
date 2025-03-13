e7234476737d0630e8c635ed290ea37529e004a4
Jim Rutt <jimrutt@gmail.com>
2025-03-13 13:00:19 -0400

    all window numeric keys working in editors.  text at top

--- lib/widgets/text_editor_with_nav.dart
+++ lib/widgets/text_editor_with_nav.dart
@@ -3,24 +3,20 @@ import 'package:flutter/services.dart';
 
 class TextEditorWithNav extends StatefulWidget {
   final TextEditingController controller;
-  final FocusNode? focusNode;
+  final bool autofocus;
+  final TextStyle? style;
   final InputDecoration? decoration;
   final int? maxLines;
   final bool expands;
-  final TextAlignVertical? textAlignVertical;
-  final TextStyle? style;
-  final bool autofocus;
 
   const TextEditorWithNav({
     super.key,
     required this.controller,
-    this.focusNode,
+    this.autofocus = false,
+    this.style,
     this.decoration,
     this.maxLines,
     this.expands = false,
-    this.textAlignVertical,
-    this.style,
-    this.autofocus = false,
   });
 
   @override
@@ -28,264 +24,149 @@ class TextEditorWithNav extends StatefulWidget {
 }
 
 class _TextEditorWithNavState extends State<TextEditorWithNav> {
-  late FocusNode _focusNode;
-  final FocusNode _rawKeyboardFocusNode = FocusNode();
+  final FocusNode _textFieldFocusNode = FocusNode();
+  final FocusNode _keyboardListenerFocusNode = FocusNode();
 
   @override
   void initState() {
     super.initState();
-    _focusNode = widget.focusNode ?? FocusNode();
-    
     if (widget.autofocus) {
       WidgetsBinding.instance.addPostFrameCallback((_) {
-        _focusNode.requestFocus();
+        _textFieldFocusNode.requestFocus();
       });
     }
   }
 
   @override
   void dispose() {
-    if (widget.focusNode == null) {
-      _focusNode.dispose();
-    }
-    _rawKeyboardFocusNode.dispose();
+    _textFieldFocusNode.dispose();
+    _keyboardListenerFocusNode.dispose();
     super.dispose();
   }
 
-  void _handleKeyEvent(RawKeyEvent event) {
-    if (event is RawKeyDownEvent) {
-      // Get current cursor position and text
-      final TextEditingValue value = widget.controller.value;
-      
-      // Handle case where selection might be invalid
-      if (value.selection.baseOffset < 0) {
-        return;
-      }
+  void _handleArrowKey(LogicalKeyboardKey key) {
+    final selection = widget.controller.selection;
+    final text = widget.controller.text;
+    final lines = text.split('\n');
+    int currentLine = 0;
+    int currentColumn = selection.start;
 
-      final int cursorPos = value.selection.baseOffset;
-      final String text = value.text;
-      int newPos = cursorPos;
-
-      // Handle different numpad keys
-      if (event.physicalKey == PhysicalKeyboardKey.numpad4 ||
-          (event.physicalKey == PhysicalKeyboardKey.arrowLeft && 
-           event.logicalKey == LogicalKeyboardKey.arrowLeft)) {
-        // Left arrow - move cursor one character left
-        if (cursorPos > 0) {
-          newPos = cursorPos - 1;
-        }
-      } else if (event.physicalKey == PhysicalKeyboardKey.numpad6 ||
-                (event.physicalKey == PhysicalKeyboardKey.arrowRight && 
-                 event.logicalKey == LogicalKeyboardKey.arrowRight)) {
-        // Right arrow - move cursor one character right
-        if (cursorPos < text.length) {
-          newPos = cursorPos + 1;
-        }
-      } else if (event.physicalKey == PhysicalKeyboardKey.numpad8 ||
-                (event.physicalKey == PhysicalKeyboardKey.arrowUp && 
-                 event.logicalKey == LogicalKeyboardKey.arrowUp)) {
-        // Up arrow - move cursor to previous line at similar horizontal position
-        if (text.isEmpty) return;
-
-        try {
-          // Get all lines in the text
-          List<String> lines = text.split('\n');
-          
-          // Find which line we're currently on
-          int currentLineIndex = 0;
-          int charCount = 0;
-          
-          // Find the current line by counting characters
-          for (int i = 0; i < lines.length; i++) {
-            if (charCount + lines[i].length >= cursorPos) {
-              currentLineIndex = i;
-              break;
-            }
-            // Add 1 for the newline character
-            charCount += lines[i].length + 1;
-          }
-          
-          // If we're on the first line, can't go up
-          if (currentLineIndex <= 0) return;
-          
-          // Calculate column position in current line
-          int columnInCurrentLine = cursorPos - charCount;
-          
-          // Calculate position at start of previous line
-          int prevLineStart = 0;
-          for (int i = 0; i < currentLineIndex - 1; i++) {
-            prevLineStart += lines[i].length + 1; // +1 for newline
-          }
-          
-          // Calculate position in previous line
-          int prevLineLength = lines[currentLineIndex - 1].length;
-          int columnInPrevLine = columnInCurrentLine < prevLineLength
-              ? columnInCurrentLine
-              : prevLineLength;
-          
-          // Set new position
-          newPos = prevLineStart + columnInPrevLine;
-        } catch (e) {
-          // Fallback: just try to find the previous newline
-          if (cursorPos > 0) {
-            final int lastNewlineIndex = text.lastIndexOf('\n', cursorPos - 1);
-            if (lastNewlineIndex >= 0) {
-              // Find the newline before that one
-              final int prevNewlineIndex = text.lastIndexOf('\n', lastNewlineIndex - 1);
-              if (prevNewlineIndex >= 0) {
-                // Try to maintain the same column position
-                final int currentColumn = cursorPos - lastNewlineIndex - 1;
-                final int prevLineLength = lastNewlineIndex - prevNewlineIndex - 1;
-                newPos = prevNewlineIndex + 1 + (currentColumn < prevLineLength ? currentColumn : prevLineLength);
-              } else {
-                // We're moving to the first line
-                final int currentColumn = cursorPos - lastNewlineIndex - 1;
-                newPos = currentColumn < lastNewlineIndex ? currentColumn : lastNewlineIndex;
-              }
-            }
-          }
-        }
-      } else if (event.physicalKey == PhysicalKeyboardKey.numpad2 ||
-                (event.physicalKey == PhysicalKeyboardKey.arrowDown && 
-                 event.logicalKey == LogicalKeyboardKey.arrowDown)) {
-        // Down arrow - move cursor to next line at similar horizontal position
-        if (text.isEmpty) return;
-
-        try {
-          // Get all lines in the text
-          List<String> lines = text.split('\n');
-          
-          // Find which line we're currently on
-          int currentLineIndex = 0;
-          int charCount = 0;
-          
-          // Find the current line by counting characters
-          for (int i = 0; i < lines.length; i++) {
-            if (charCount + lines[i].length >= cursorPos) {
-              currentLineIndex = i;
-              break;
-            }
-            // Add 1 for the newline character
-            charCount += lines[i].length + 1;
-          }
-          
-          // If we're on the last line, can't go down
-          if (currentLineIndex >= lines.length - 1) return;
-          
-          // Calculate column position in current line
-          int columnInCurrentLine = cursorPos - charCount;
-          
-          // Calculate position at start of next line
-          int nextLineStart = 0;
-          for (int i = 0; i <= currentLineIndex; i++) {
-            nextLineStart += lines[i].length + 1; // +1 for newline
-          }
-          
-          // Calculate position in next line
-          int nextLineLength = lines[currentLineIndex + 1].length;
-          int columnInNextLine = columnInCurrentLine < nextLineLength
-              ? columnInCurrentLine
-              : nextLineLength;
-          
-          // Set new position
-          newPos = nextLineStart + columnInNextLine;
-        } catch (e) {
-          // Fallback: just try to find the next newline and move after it
-          final int nextLineStart = text.indexOf('\n', cursorPos);
-          if (nextLineStart >= 0 && nextLineStart + 1 < text.length) {
-            newPos = nextLineStart + 1;
-          }
-        }
-      } else if (event.physicalKey == PhysicalKeyboardKey.numpad7 ||
-                (event.physicalKey == PhysicalKeyboardKey.home && 
-                 event.logicalKey == LogicalKeyboardKey.home)) {
-        // Home - move cursor to start of line
-        if (text.isEmpty) return;
-
-        if (cursorPos > 0) {
-          final int lastNewlineIndex = text.lastIndexOf('\n', cursorPos - 1);
-          newPos = lastNewlineIndex >= 0 ? lastNewlineIndex + 1 : 0;
-        } else {
-          newPos = 0;
-        }
-      } else if (event.physicalKey == PhysicalKeyboardKey.numpad1 ||
-                (event.physicalKey == PhysicalKeyboardKey.end && 
-                 event.logicalKey == LogicalKeyboardKey.end)) {
-        // End - move cursor to end of line
-        if (text.isEmpty) return;
-
-        final int nextNewline = text.indexOf('\n', cursorPos);
-        newPos = nextNewline >= 0 ? nextNewline : text.length;
-      } else if (event.physicalKey == PhysicalKeyboardKey.numpad9 ||
-                (event.physicalKey == PhysicalKeyboardKey.pageUp && 
-                 event.logicalKey == LogicalKeyboardKey.pageUp)) {
-        // Page Up - move cursor up several lines
-        if (text.isEmpty) return;
-
-        int linesUp = 10;
-        int tempPos = cursorPos;
-        while (linesUp > 0 && tempPos > 0) {
-          final int prevNewline = text.lastIndexOf('\n', tempPos - 1);
-          if (prevNewline >= 0) {
-            tempPos = prevNewline;
-            linesUp--;
-          } else {
-            break;
-          }
-        }
-        newPos = tempPos > 0 ? tempPos + 1 : 0;
-      } else if (event.physicalKey == PhysicalKeyboardKey.numpad3 ||
-                (event.physicalKey == PhysicalKeyboardKey.pageDown && 
-                 event.logicalKey == LogicalKeyboardKey.pageDown)) {
-        // Page Down - move cursor down several lines
-        if (text.isEmpty) return;
-
-        int linesDown = 10;
-        int tempPos = cursorPos;
-        while (linesDown > 0 && tempPos < text.length) {
-          final int nextNewline = text.indexOf('\n', tempPos);
-          if (nextNewline >= 0) {
-            tempPos = nextNewline + 1;
-            linesDown--;
-          } else {
-            tempPos = text.length;
-            break;
-          }
-        }
-        newPos = tempPos;
-      }
-
-      // Update cursor position if it changed
-      if (newPos != cursorPos) {
-        widget.controller.selection =
-            TextSelection.fromPosition(TextPosition(offset: newPos));
-        return;
+    // Find current line and column
+    for (var i = 0; i < lines.length; i++) {
+      if (currentColumn > lines[i].length) {
+        currentColumn -= lines[i].length + 1; // +1 for newline
+        currentLine++;
+      } else {
+        break;
       }
     }
+
+    int newOffset = selection.start;
+
+    switch (key) {
+      case LogicalKeyboardKey.arrowLeft:
+        if (selection.start > 0) {
+          newOffset = selection.start - 1;
+        }
+        break;
+      case LogicalKeyboardKey.arrowRight:
+        if (selection.start < text.length) {
+          newOffset = selection.start + 1;
+        }
+        break;
+      case LogicalKeyboardKey.arrowUp:
+        if (currentLine > 0) {
+          final previousLineLength = lines[currentLine - 1].length;
+          final previousLineStart = _getLineStart(text, currentLine - 1);
+          newOffset = previousLineStart +
+              (currentColumn < previousLineLength
+                  ? currentColumn
+                  : previousLineLength);
+        }
+        break;
+      case LogicalKeyboardKey.arrowDown:
+        if (currentLine < lines.length - 1) {
+          final nextLineLength = lines[currentLine + 1].length;
+          final nextLineStart = _getLineStart(text, currentLine + 1);
+          newOffset = nextLineStart +
+              (currentColumn < nextLineLength ? currentColumn : nextLineLength);
+        }
+        break;
+      case LogicalKeyboardKey.home:
+        newOffset = _getLineStart(text, currentLine);
+        break;
+      case LogicalKeyboardKey.end:
+        newOffset =
+            _getLineStart(text, currentLine) + lines[currentLine].length;
+        break;
+      default:
+        return;
+    }
+
+    widget.controller.selection = TextSelection.collapsed(offset: newOffset);
+  }
+
+  void _handleDelete() {
+    final selection = widget.controller.selection;
+    final text = widget.controller.text;
+
+    // Check if we have a valid selection and text
+    if (selection.start < 0 || selection.start >= text.length) {
+      return;
+    }
+
+    if (selection.start < text.length) {
+      final newText =
+          text.replaceRange(selection.start, selection.start + 1, '');
+      widget.controller.value = TextEditingValue(
+        text: newText,
+        selection: TextSelection.collapsed(offset: selection.start),
+      );
+    }
+  }
+
+  int _getLineStart(String text, int lineNumber) {
+    final lines = text.split('\n');
+    int offset = 0;
+    for (var i = 0; i < lineNumber; i++) {
+      offset += lines[i].length + 1; // +1 for newline
+    }
+    return offset;
   }
 
   @override
   Widget build(BuildContext context) {
     return RawKeyboardListener(
-      focusNode: _rawKeyboardFocusNode,
-      onKey: _handleKeyEvent,
+      focusNode: _keyboardListenerFocusNode,
+      onKey: (RawKeyEvent event) {
+        if (event is RawKeyDownEvent) {
+          if (event.physicalKey == PhysicalKeyboardKey.numpad4) {
+            _handleArrowKey(LogicalKeyboardKey.arrowLeft);
+          } else if (event.physicalKey == PhysicalKeyboardKey.numpad6) {
+            _handleArrowKey(LogicalKeyboardKey.arrowRight);
+          } else if (event.physicalKey == PhysicalKeyboardKey.numpad8) {
+            _handleArrowKey(LogicalKeyboardKey.arrowUp);
+          } else if (event.physicalKey == PhysicalKeyboardKey.numpad2) {
+            _handleArrowKey(LogicalKeyboardKey.arrowDown);
+          } else if (event.physicalKey == PhysicalKeyboardKey.numpad7) {
+            _handleArrowKey(LogicalKeyboardKey.home);
+          } else if (event.physicalKey == PhysicalKeyboardKey.numpad1) {
+            _handleArrowKey(LogicalKeyboardKey.end);
+          } else if (event.physicalKey == PhysicalKeyboardKey.numpadDecimal) {
+            _handleDelete();
+          }
+        }
+      },
       child: TextField(
         controller: widget.controller,
-        focusNode: _focusNode,
+        focusNode: _textFieldFocusNode,
+        style: widget.style,
+        decoration: widget.decoration,
         maxLines: widget.maxLines,
         expands: widget.expands,
-        textAlignVertical: widget.textAlignVertical ?? TextAlignVertical.top,
         keyboardType: TextInputType.multiline,
-        enableInteractiveSelection: true,
-        textInputAction: TextInputAction.newline,
-        enableSuggestions: true,
-        style: widget.style,
-        decoration: widget.decoration?.copyWith(
-          alignLabelWithHint: true,
-        ) ?? const InputDecoration(
-          alignLabelWithHint: true,
-        ),
+        textAlignVertical: TextAlignVertical.top,
       ),
     );
   }
-} 
+}
